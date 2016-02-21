423f378c1379f208634287f72625720581bd18b2
Brendan Whiting <brendan.whiting@me.com>
2016-02-21 02:36:48 -0500

    outside function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-    $.getJSON('video/captions.txt', function(data) {         
-        console.log(data);
-        
-        $('#text').html(data);
-    });
-    console.log("hey!");
+    var JSONcaptions = $.getJSON('video/captions.txt'), 
+    console.log(JSONcaptions);
 }); 
