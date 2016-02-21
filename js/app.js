b4a30056ba595e02582b73b6d5cf2375dc691504
Brendan Whiting <brendan.whiting@me.com>
2016-02-21 02:33:59 -0500

    hey

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $.getJSON('video/captions.txt', function(data) {         
         console.log(data);
-        console.log("hey!");
+        
         $('#text').html(data);
     });
+    console.log("hey!");
 }); 
