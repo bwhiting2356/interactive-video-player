581517dadd00da92aeb7378976fd07c205a1e648
Brendan Whiting <brendan.whiting@me.com>
2016-02-22 03:36:21 -0500

    ...

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+function parseCaptionText(text) {
+    console.log(text.split());
+}
+
+
 $.get('video/captions.txt', function(data) {
-   alert(data);
+   parseCaptionText(data);
 });
 
 
-$(document).ready(function() {
-    // var response = $.getJSON('video/captions.txt'); 
-    // var JSONcaptions = response.responseText;
-    // // console.log(response, JSONcaptions);
-    // parseCaptionText(caption_text);
-}); 
+// $(document).ready(function() {
+//     // var response = $.getJSON('video/captions.txt'); 
+//     // var JSONcaptions = response.responseText;
+//     // // console.log(response, JSONcaptions);
+//     // parseCaptionText(caption_text);
+// }); 
